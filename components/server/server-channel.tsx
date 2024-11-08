56ec5f504a4d0ec7cac95db2c07d8c14469c5f1c
v-chenx83 <1614819989@qq.com>
2024-11-08 12:51:56 +0800

    完善频道列表功能

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -6,7 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { ActionTooltip } from "@/components/action-tooltip";
-import { useModal } from "@/hooks/use-model-store";
+import { ModalType, useModal } from "@/hooks/use-model-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -33,9 +33,22 @@ export const ServerChannel = ({
 
   const Icon = iconMap[channel.type];
 
+  // 跳转到指定频道
+  const onClick = () => {
+    router.push(`/server/${params.serverId}/channels/${channel.id}`)
+  }
+
+  // 防止事件冒泡
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { server, channel });
+  }
+
+
+
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         "group py-2 px-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -52,13 +65,13 @@ export const ServerChannel = ({
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="编辑">
             <Edit 
-              onClick={() => onOpen("editChannel", { server, channel })}
+              onClick={(e) => onAction(e, "editChannel")}
               className="hidden group-hover:block w-4 h-5 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="删除">
             <Trash
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => onAction(e,"deleteChannel")}
               className="hidden group-hover:block w-4 h-5 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
